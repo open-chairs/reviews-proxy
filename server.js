2f476e310fe7eec1244f83dcadff0d6c2d7691f3
Adrienne Lim <adriennelim.mc@gmail.com>
2019-04-30 16:54:00 -0700

    refactor proxy to get data from localhost:3004

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 var express = require('express');
-var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
 var port = process.env.port || 3005;
 
-app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/reviews/*', (req, res) => {
+    axios[req.method.toLowerCase()](`http://localhost:3004${req.originalUrl}`)
+    .then(({data}) => {
+        res.send(data)})
+    .catch(err => res.statusCode(500).send(err))
+});
+
+app.use('*', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
 	console.log(`Express proxy server running at http://localhost:${port}`);
